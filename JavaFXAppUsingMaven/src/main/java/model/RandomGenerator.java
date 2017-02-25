0739d629ccbe141e6bc063e0d0932e05b9fff53d
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-25 15:30:49 +0100

    license header

---
+++ JavaFXAppUsingMaven/src/main/java/model/RandomGenerator.java
@@ -0,0 +1,28 @@
+/*
+ * String_Generator - Text analysis for realistic random strings generation
+ * 
+ * RandomGenerator.java
+ * Copyright (C) 2017 Jarretier Adrien
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package model;
+
+/**
+ *
+ * @author Jarretier Adrien "jarretier.adrien@gmail.com"
+ */
+public class RandomGenerator {
+    
+}
