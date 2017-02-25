7ea4c747d0b6cc9e1e123b05f688c926f0aa758c
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-25 04:40:29 +0100

    using ssj to generate test pseudo random number in helloWorld window

--- JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
+++ JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
@@ -1,25 +1,42 @@
 package com.mycompany.javafxappusingmaven;
 
 import java.net.URL;
+import java.sql.Time;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.stream.LongStream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import umontreal.ssj.rng.MRG32k3a;
+import umontreal.ssj.rng.MT19937;
 
 public class FXMLController implements Initializable {
-    
+
+    private MT19937 rng;
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
+        label.setText(String.valueOf(rng.nextInt(1, 10)));
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+        Random rand = new Random();
+        LongStream seed = LongStream.concat(rand.longs(3, 1, 4294967087L), rand.longs(3, 1, 4294944443L));
+
+        MRG32k3a.setPackageSeed(seed.toArray());
+
+        MRG32k3a mrg = new MRG32k3a();
+
+        System.out.println(mrg.nextDouble());
+        rng = new MT19937(mrg);
+
+    }
 }
