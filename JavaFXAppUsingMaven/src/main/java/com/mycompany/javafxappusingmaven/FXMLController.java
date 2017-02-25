c72989946eb3f283510e2326b2f560d3c5f05965
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-25 16:33:12 +0100

    unsing RandomGenerator

--- JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
+++ JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
@@ -20,15 +20,12 @@
 package com.mycompany.javafxappusingmaven;
 
 import java.net.URL;
-import java.sql.Time;
-import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.stream.LongStream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import umontreal.ssj.rng.MRG32k3a;
+import model.RandomGenerator;
 import umontreal.ssj.rng.MT19937;
 
 public class FXMLController implements Initializable {
@@ -41,21 +38,11 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
 
-        label.setText(String.valueOf(rng.nextInt(1, 10)));
+        label.setText(String.valueOf(RandomGenerator.getGenerator().rollI(0, 10)));
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        Random rand = new Random();
-        LongStream seed = LongStream.concat(rand.longs(3, 1, 4294967087L), rand.longs(3, 1, 4294944443L));
-
-        MRG32k3a.setPackageSeed(seed.toArray());
-
-        MRG32k3a mrg = new MRG32k3a();
-
-        System.out.println(mrg.nextDouble());
-        rng = new MT19937(mrg);
 
     }
 }
