389daf8c7ddc4208db352e07cdbb1531ee45feb8
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-27 19:16:39 +0100

    removed unused imports

--- JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
+++ JavaFXAppUsingMaven/src/main/java/com/mycompany/javafxappusingmaven/FXMLController.java
@@ -23,8 +23,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
