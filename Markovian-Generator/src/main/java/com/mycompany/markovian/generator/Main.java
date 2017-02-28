c4c9c2464a24817797524543ef420ab976022095
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-28 12:38:49 +0100

    generating words with default parameters in javafx window

--- Markovian-Generator/src/main/java/com/mycompany/markovian/generator/Main.java
+++ Markovian-Generator/src/main/java/com/mycompany/markovian/generator/Main.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import model.RandomString;
 
@@ -23,46 +25,10 @@ public class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        launch(args);
 
-//        String WORDS_LIST_FOLDER = "resources/words-lists";
-//        String ENGLISH_WORDS = WORDS_LIST_FOLDER + "/english.txt";
-//        String FRENCH_WORDS = WORDS_LIST_FOLDER + "/liste.de.mots.francais.frgut.txt";
-//        String ENGLISH_SHORT = WORDS_LIST_FOLDER + "/shortEnglish.txt";
-//
-//        int ORDER = 3;
-//
-//        System.out.println("Analysing language for generation of a " + ORDER + "-order Markov Chain...");
-//
-//        RandomString rs;
-//        try {
-//            rs = new RandomString(ENGLISH_WORDS, ORDER);
-//
-//            System.out.println("Done.");
-//
-//            int minLength, maxLength;
-//
-//            System.out.print("Min length : ");
-//            Scanner sc = new Scanner(System.in);
-//
-//            minLength = sc.nextInt();
-//            System.out.print("Max length : ");
-//            maxLength = sc.nextInt();
-//
-//            System.out.println("Rolling 16 words :");
-//
-//            for (int i = 0; i < 16; ++i) {
-//                StringBuilder rolled = new StringBuilder(rs.roll(minLength, maxLength));
-//                rolled.setCharAt(0, Character.toUpperCase(rolled.charAt(0)));
-//
-//                System.out.println(rolled);
-//            }
-//        } catch (IOException ex) {
-//            System.err.println(ex.getMessage());
-//        }
+        launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Markovian Generator");
@@ -70,11 +36,55 @@ public class Main extends Application {
         // en top, left, right, bottom and center
         BorderPane root = new BorderPane();
 
-        // main window fixed size of 1280 x 720p
-        Scene scene = new Scene(root, 1280, 720);
-        
+        // main window fixed size
+        Scene scene = new Scene(root, 800, 600);
+
+        TextFlow mainText = new TextFlow();
+        root.setCenter(mainText);
+
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        String WORDS_LIST_FOLDER = "resources/words-lists";
+        String ENGLISH_WORDS = WORDS_LIST_FOLDER + "/english.txt";
+        String FRENCH_WORDS = WORDS_LIST_FOLDER + "/liste.de.mots.francais.frgut.txt";
+        String ENGLISH_SHORT = WORDS_LIST_FOLDER + "/shortEnglish.txt";
+        String TOLKIEN = WORDS_LIST_FOLDER + "/tolkiensCharacters.txt";
+
+        int ORDER = 3;
+
+        mainText.getChildren().add(new Text("Analysing list for generation of a " + ORDER + "-order Markov Chain..."));
+        mainText.getChildren().add(new Text(System.getProperty("line.separator")));
+
+        try {
+
+            RandomString rs = new RandomString(TOLKIEN, ORDER);
+
+            System.out.println("Done.");
+
+            int minLength, maxLength;
+
+//            System.out.print("Min length : ");
+//            Scanner sc = new Scanner(System.in);
+//            minLength = sc.nextInt();
+//            System.out.print("Max length : ");
+//            maxLength = sc.nextInt();
+            minLength = 1;
+            maxLength = 24;
+
+            mainText.getChildren().add(new Text("Rolling 16 words :"));
+            mainText.getChildren().add(new Text(System.getProperty("line.separator")));
+
+            for (int i = 0; i < 16; ++i) {
+                StringBuilder rolled = new StringBuilder(rs.roll(minLength, maxLength));
+                rolled.setCharAt(0, Character.toUpperCase(rolled.charAt(0)));
+
+                mainText.getChildren().add(new Text(rolled.toString()));
+                mainText.getChildren().add(new Text(System.getProperty("line.separator")));
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
-    
+
 }
